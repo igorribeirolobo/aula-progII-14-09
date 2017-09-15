ad557d5e3d292fffe905258827da1a5edbd44815
Igor Ribeiro Lobo <igorribeirolobo@gmail.com>
2017-09-14 21:09:52 -0300

    inicio aula

---
+++ src/br/estacio/progII/Main.java
@@ -0,0 +1,10 @@
+package br.estacio.progII;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/br/estacio/progII/funcao.java
@@ -0,0 +1,5 @@
+package br.estacio.progII;
+
+public class funcao {
+
+}
